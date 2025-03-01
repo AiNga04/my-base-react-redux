a1ea42a8e144f57c2e3939100da2c6b8e1d36340
AiNga04 <aingaspkt@gmail.com>
2025-03-02 00:17:38 +0700

    build source base

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+const App = () => {
+  return (
+    <div className="app-container">
+      <h2 className="text-primary">hello world</h2>
+    </div>
+  );
+};
+
+export default App;

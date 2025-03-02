3f3e67c71a22e3d7f2d410dd19bd5dd5ba219bd3
AiNga04 <aingaspkt@gmail.com>
2025-03-02 22:54:12 +0700

    feat(jsx): Add site navigation

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <NavLink className="navbar-brand" to="/abouts">
+          Zyna Dev
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/abouts">
+              About
+            </NavLink>
+            <NavLink className="nav-link" to="/users">
+              User
+            </NavLink>
+            <NavLink className="nav-link" to="/admins">
+              Admin
+            </NavLink>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item>Log in</NavDropdown.Item>
+              <NavDropdown.Item>Log out</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
